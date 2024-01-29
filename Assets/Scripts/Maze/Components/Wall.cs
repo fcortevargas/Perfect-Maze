1a7b54d7b35108e26064b5f88033a46b491ce68b
fcortevargas <f.cortevargas@gmail.com>
2024-01-29 22:27:44 +0100

    :bulb: Added comments to the Wall.cs class.

--- Assets/Scripts/Maze/Components/Wall.cs
+++ Assets/Scripts/Maze/Components/Wall.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
 
+// The Wall class represents a wall in a maze
 public class Wall
 {
-    public Cell Cell1;
-    public Cell Cell2;
-    public float Weight;
-    public GameObject GameObject;
+    // Cell1 and Cell2 represent the two cells that this wall separates
+    public readonly Cell Cell1; 
+    public readonly Cell Cell2; 
 
+    // Weight of the wall, used in the maze generation algorithm
+    public readonly float Weight;
+
+    // The GameObject associated with this wall
+    public readonly GameObject GameObject;
+
+    // Constructor to initialize a Wall instance
     public Wall(Cell c1, Cell c2, GameObject gameObject)
     {
-        Cell1 = c1;
-        Cell2 = c2;
-        Weight = Random.Range(0f, 1f);
-        GameObject = gameObject;
+        Cell1 = c1; // Assign the first adjacent cell
+        Cell2 = c2; // Assign the second adjacent cell
+        Weight = Random.Range(0f, 1f); // Assign a random weight to the wall for maze generation algorithms
+        GameObject = gameObject; // Assign the Unity GameObject that represents this wall visually
     }
 }

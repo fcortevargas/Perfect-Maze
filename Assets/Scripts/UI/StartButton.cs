521e52d37b26143955a1036fee2554b94fc5ae4f
fcortevargas <f.cortevargas@gmail.com>
2024-01-29 22:27:33 +0100

    :bulb: Added comments to the StartButton.cs script.

--- Assets/Scripts/UI/StartButton.cs
+++ Assets/Scripts/UI/StartButton.cs
@@ -1,22 +1,28 @@
-using UnityEngine;
+using UI.Base_Classes;
 
 namespace UI
 {
+    // The StartButton class extends ButtonController
     public class StartButton : ButtonController
     {
         private void Start()
         {
+            // If the Maze scene is not loaded yet, add a listener to the button's onClick event to load the Maze scene
             if (!GameManager.IsMazeSceneLoaded)
                 Button.onClick.AddListener(GameManager.Instance.LoadMaze);
+            // If the Maze scene is already loaded, add a listener to start disabling the walls in the Maze
             else
                 Button.onClick.AddListener(Maze.Instance.StartDisablingWalls);
         }
-
+        
+        // This method defines the conditions under which the button is interactable
         protected override bool IsInteractable()
         {
+            // If the Maze instance is not available, the button is interactable only if the maze generation speed is set
             if (Maze.Instance == null)
                 return GameManager.MazeSpeed > 0;
             
+            // If the Maze instance is available, the button is interactable if the maze is in a reset state
             return GameManager.IsMazeReset;
         }
     }

ed8f56e93bc31661ee9feeb3d6b38fea9c599bf9
fcortevargas <f.cortevargas@gmail.com>
2024-01-29 22:26:42 +0100

    :bulb: Added comments to the MainMenuButton.cs script.

--- Assets/Scripts/UI/MainMenuButton.cs
+++ Assets/Scripts/UI/MainMenuButton.cs
@@ -1,18 +1,21 @@
+using UI.Base_Classes;
+
 namespace UI
 {
+    // MainMenuButton class extends ButtonController
     public class MainMenuButton : ButtonController
     {
+        // Start method is called when the script instance is being loaded
         private void Start()
         {
+            // This sets up the button to trigger loading the main menu scene when clicked
             Button.onClick.AddListener(GameManager.Instance.LoadMainMenu);
         }
 
         protected override bool IsInteractable()
         {
+            // The main menu button is always interactable
             return true;
         }
     }
 }
-
-
-

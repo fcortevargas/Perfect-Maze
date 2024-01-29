f739866d32b4a18ba8b9fee5b477ace45c42714a
fcortevargas <f.cortevargas@gmail.com>
2024-01-29 22:26:30 +0100

    :bulb: Added comments to the HeighSlider.cs script.

--- Assets/Scripts/UI/HeightSlider.cs
+++ Assets/Scripts/UI/HeightSlider.cs
@@ -1,9 +1,13 @@
+using UI.Base_Classes;
+
 namespace UI
 {
+    // SpeedSlider class extends SliderController
     public class HeightSlider : SliderController
     {
         protected override void HandleSliderValueChanged(float value)
         {
+            // Set the MazeHeight in the GameManager to the current value of the slider
             GameManager.MazeHeight = (int)value;
         }
     }

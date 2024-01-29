1cc56f790365f5169381e4c95db571c7635c5ff7
fcortevargas <f.cortevargas@gmail.com>
2024-01-29 22:27:11 +0100

    :bulb: Added comments to the ResetButton.cs script.

--- Assets/Scripts/UI/ResetButton.cs
+++ Assets/Scripts/UI/ResetButton.cs
@@ -1,14 +1,20 @@
+using UI.Base_Classes;
+
 namespace UI
 {
+    // ResetButton class extends ButtonController
     public class ResetButton : ButtonController
     { 
         private void Start()
         {
+            // If the Maze scene is loaded, add a listener to the button's onClick event to call the ResetMaze method in the Maze instance
             if (GameManager.IsMazeSceneLoaded)
                 Button.onClick.AddListener(Maze.Instance.ResetMaze);
         }
+        
         protected override bool IsInteractable()
         {
+            // The button is interactable only if the maze is completed and has not been reset yet
             return GameManager.IsMazeCompleted && !GameManager.IsMazeReset;
         }
     }

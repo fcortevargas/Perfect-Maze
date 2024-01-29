71b63f788097784358a932d1379eb0476b77636e
fcortevargas <f.cortevargas@gmail.com>
2024-01-29 22:27:26 +0100

    :bulb: Added comments to the SpeedSlider.cs script.

--- Assets/Scripts/UI/SpeedSlider.cs
+++ Assets/Scripts/UI/SpeedSlider.cs
@@ -1,9 +1,13 @@
+using UI.Base_Classes;
+
 namespace UI
 {
+    // SpeedSlider class extends SliderController
     public class SpeedSlider : SliderController
     {
         protected override void HandleSliderValueChanged(float value)
         {
+            // Set the MazeSpeed in the GameManager to the current value of the slider
             GameManager.MazeSpeed = value;
         }
     }

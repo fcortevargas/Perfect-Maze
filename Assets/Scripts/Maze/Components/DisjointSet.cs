464941a0387fc3c90cbf0937cff27a0a8b05fdf9
fcortevargas <f.cortevargas@gmail.com>
2024-01-29 22:25:55 +0100

    :bulb: Added comments to the DisjointSet.cs script.

--- Assets/Scripts/Maze/Components/DisjointSet.cs
+++ Assets/Scripts/Maze/Components/DisjointSet.cs
@@ -1,30 +1,42 @@
+// DisjointSet is a data structure that implements the union-find algorithm
 public class DisjointSet
 {
+    // Array to hold the parent of each element in the disjoint set
     private readonly int[] _parent;
 
+    // Constructor to initialize the disjoint set
     public DisjointSet(int count)
     {
+        // Initialize the _parent array with the specified number of elements (count)
         _parent = new int[count];
+
+        // Initially, each element is its own parent, forming 'count' number of singleton sets
         for (var i = 0; i < count; i++)
         {
             _parent[i] = i;
         }
     }
 
+    // Find method to find the root (or representative) of the set
     public int Find(int i)
     {
+        // Follow the chain of parent pointers until the root is reached
         while (i != _parent[i])
         {
             i = _parent[i];
         }
         
-        return i;
+        return i; 
     }
 
+    // Union method to merge two sets
     public void Union(int x, int y)
     {
+        // Find the roots of the sets that x and y belong to
         var xRoot = Find(x);
         var yRoot = Find(y);
+
+        // Make the root of x's set point to the root of y's set, effectively merging the two sets
         _parent[xRoot] = yRoot;
     }
 }

1299e661ba3ee6ee90f53a8197d3d063be00d3a9
fcortevargas <f.cortevargas@gmail.com>
2024-01-29 22:24:57 +0100

    :bulb: Added comments to the Cell.cs class.

--- Assets/Scripts/Maze/Components/Cell.cs
+++ Assets/Scripts/Maze/Components/Cell.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 
+// The Cell class represents an individual cell in a maze
 public class Cell
 {
+    // X and Y are the coordinates of the cell in the maze grid
     public readonly int X;
     public readonly int Y;
+
+    // GameObject associated with this cell
     public GameObject GameObject;
 
+    // Constructor to initialize a Cell instance
     public Cell(int x, int y, GameObject gameObject)
     {
-        X = x;
+        X = x; 
         Y = y;
         GameObject = gameObject;
     }

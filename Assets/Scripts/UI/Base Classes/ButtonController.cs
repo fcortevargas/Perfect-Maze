493bd18ddb13853b553366c650dc41b3fdce3413
fcortevargas <f.cortevargas@gmail.com>
2024-01-29 22:25:42 +0100

    :bulb: Added comments to the ButtonController.cs script.

--- Assets/Scripts/UI/Base Classes/ButtonController.cs
+++ Assets/Scripts/UI/Base Classes/ButtonController.cs
@@ -1,22 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace UI
+namespace UI.Base_Classes
 {
+    // ButtonController is an abstract class that provides a base implementation for button controls in the UI
     public abstract class ButtonController : MonoBehaviour
     {
+        // Protected field for the Button component
         protected Button Button;
         
         private void Awake()
         {
             Button = GetComponent<Button>();
         }
-    
+        
         private void Update()
         {
+            // Calls the abstract IsInteractable method, which is implemented in derived classes
             Button.interactable = IsInteractable(); 
         }
-
+        
+        // Abstract method to determine whether the button is interactable based on custom conditions
         protected abstract bool IsInteractable();
     }
 }
